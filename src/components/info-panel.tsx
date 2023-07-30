6a5a853c2da40247abf2dd6db4dadccf81fca216
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-07-30 20:41:55 +0100

    Info panel
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

---
+++ src/components/info-panel.tsx
@@ -0,0 +1,159 @@
+import { Alert, Button, Grid, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+
+interface StringMap {[key: string]: string};
+
+export const InfoPanel = ({initial, updateHandler}) => {
+
+    const [metadata, setMetadata] = useState(initial);
+    const [metadataFieldName, setMetadataFieldName] = useState('');
+    const [metadataFieldValue, setMetadataFieldValue] = useState('');
+    const [extraFields, setExtraFields] = useState<StringMap>({}); 
+    const [alert, setAlert] = useState('');
+    
+    useEffect(() => {
+        const knownFields = ['name', 'pre_description', 'project_lead', 'lead_institution'];
+        const unknownFields = Object.keys(metadata).filter((key) => !knownFields.includes(key));
+        const newExtraFields = {};
+        unknownFields.forEach((key) => {
+            newExtraFields[key] = metadata[key];
+        });
+        setExtraFields(newExtraFields);
+    }, [metadata]);
+
+    const setProp = (prop: string, value: string) => {
+        const newMetadata = Object.assign({}, metadata);
+        newMetadata[prop] = value;
+        updateHandler(newMetadata);
+        setMetadata(newMetadata);
+    };
+
+    const updateMetadataFieldName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMetadataFieldName(event.target.value);
+    };
+
+    const updateMetadataFieldValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMetadataFieldValue(event.target.value);
+    };
+
+    const addNewMetadataField = () => { 
+        setAlert('');
+        console.log('new field', metadataFieldName, metadataFieldValue);
+        if (metadataFieldName in metadata) {
+            setAlert(`Field '${metadataFieldName}' already exists`);
+        } else {
+            setMetadataFieldName('');
+            setMetadataFieldValue('');
+            setProp(metadataFieldName, metadataFieldValue);
+        }
+    };
+
+    return (
+        <div>
+            <h2>General Information</h2>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                required
+                label="Project Name"
+                helperText="Enter a string between 2 and 100 characters long"
+                name="name"
+                value={metadata.name}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setProp('name', event.target.value);
+                }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                name="pre_description"
+                fullWidth
+                required
+                label="Project Description"
+                multiline={true}
+                rows={4}
+                value={metadata.pre_description}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setProp('pre_description', event.target.value);
+                }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                label="Project Lead"
+                name="project_lead"
+                value={metadata.project_lead}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setProp('project_lead', event.target.value);
+                }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+                fullWidth
+                label="Lead Institution"
+                name="lead_institution"
+                value={metadata.lead_institution}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setProp('lead_institution', event.target.value);
+                }}
+            />
+          </Grid>
+
+          <Grid container item xs={12} spacing={2}>
+
+            {Object.keys(extraFields).map((key) => {
+                return (
+                    <Grid item xs={12} key={key}>
+                        <TextField
+                            fullWidth
+                            label={key}
+                            name={key}
+                            value={extraFields[key]}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setProp(key, event.target.value);
+                            }}
+                        />
+                    </Grid>
+                );
+            })}
+
+            {alert && 
+            <Grid item xs={12}>
+                <Alert onClose={() => {setAlert('')}} severity="error">{alert}</Alert>
+            </Grid>}
+            
+            <Grid item xs={5}>
+            <TextField
+                fullWidth
+                label="Metadata Field Name"
+                name="metadata_field_name"
+                value={metadataFieldName}
+                onChange={updateMetadataFieldName}
+                />
+            </Grid>
+            <Grid item xs={5}>
+            <TextField
+                fullWidth
+                label="Metadata Field Value"
+                name="metadata_field_value"
+                value={metadataFieldValue}
+                onChange={updateMetadataFieldValue}
+                />
+            </Grid>
+            <Grid item xs={2}>
+            <Button 
+                variant="contained" 
+                color="primary" 
+                onClick={addNewMetadataField}>+</Button>
+            </Grid>
+          </Grid>
+
+          </Grid>
+
+        </div>
+    );
+}
--- src/components/notebook-editor.tsx
+++ src/components/notebook-editor.tsx
@@ -1,13 +1,86 @@
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Button, Grid, Tab } from "@mui/material"
+import { useState } from "react";
+import { InfoPanel } from "./info-panel";
 
+export interface NotebookType {
+    [key: string]: unknown;
+}
 
+export const NotebookEditor = ({notebook}: {notebook: NotebookType}) => {
 
-export const NotebookEditor = () => {
+    const [tabNumber, setTabNumber] = useState(1);
+    const [metadata, setMetadata] = useState(notebook.metadata);
+    const [uiSpec, setUiSpec] = useState(notebook.uiSpec);
+
+    const maxTabs = 5;
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabNumber(newValue.toString());
+    };
+
+    const nextTab = () => {
+        tabNumber < maxTabs ? setTabNumber(tabNumber + 1) : setTabNumber(maxTabs);
+    };
+
+    const previousTab = () => {
+        tabNumber > 1 ? setTabNumber((tabNumber - 1)) : setTabNumber(1);
+    };
+
+    const metadataUpdate = (metadata: any) => {
+        setMetadata(metadata);
+    };
+
+    const uiSpecUpdate = (uiSpec: any) => {
+        setUiSpec(uiSpec);
+    };
 
     return (
         <div>
             <h1>Notebook Editor</h1>
 
-            
+            <TabContext value={tabNumber.toString()}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                    <Tab label="Info" value="1" />
+                    <Tab label="Design" value="2" />
+                    <Tab label="Overview" value="3" />
+                    <Tab label="Behaviour" value="4" />
+                    <Tab label="Submit" value="5" />
+                    </TabList>
+                </Box>
+
+ 
+                <TabPanel value="1"><InfoPanel initial={metadata} updateHandler={metadataUpdate}/></TabPanel>
+                <TabPanel value="2">Design</TabPanel>
+                <TabPanel value="3">Overview</TabPanel>
+                <TabPanel value="4">Behaviour</TabPanel>
+                <TabPanel value="5">Submit</TabPanel>
+
+                <Grid 
+                    container 
+                    spacing={2}
+                    justifyContent="space-between"
+                >
+                    <Grid item>
+                        {tabNumber > 1 ?
+                        <Button variant="contained" color="primary" onClick={previousTab} >&lt; Previous</Button>
+                        : <div>&nbsp;</div>
+                        }
+                        </Grid>
+                        <Grid item>
+                        {tabNumber < maxTabs ?
+                            <Button variant="contained" color="primary" onClick={nextTab} >Next &gt;</Button>
+                        : <div>&nbsp;</div>
+                        }
+                    </Grid>
+                </Grid>
+                
+            </TabContext>
+
+            <pre className="code">
+                {JSON.stringify(metadata, null, 2)}
+            </pre>
         </div>
-    )
-}
+    );
+};
