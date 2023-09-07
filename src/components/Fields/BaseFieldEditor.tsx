9907a6761f647528bbeae56cf745df012d3a3604
EvelynChoukhman46426957 <evelyn.choukhman@students.mq.edu.au>
2023-09-07 12:50:09 +1000

    label config for annotation and uncertainty
    Signed-off-by: EvelynChoukhman46426957 <evelyn.choukhman@students.mq.edu.au>

--- src/components/Fields/BaseFieldEditor.tsx
+++ src/components/Fields/BaseFieldEditor.tsx
@@ -15,17 +15,17 @@
 import { Checkbox, FormControlLabel, Grid, TextField } from "@mui/material";
 import { useAppSelector, useAppDispatch } from "../../state/hooks";
 
-export const BaseFieldEditor = ({fieldName, children}) => {
-    
+export const BaseFieldEditor = ({ fieldName, children }) => {
+
     const field = useAppSelector(state => state['ui-specification'].fields[fieldName]);
     const dispatch = useAppDispatch();
 
     // These are needed because there is no consistency in how
     // the field label is stored in the notebook
     const getFieldLabel = () => {
-        return (field['component-parameters'] && field['component-parameters'].label) || 
-               (field['component-parameters'].InputLabelProps && field['component-parameters'].InputLabelProps.label) || 
-               field['component-parameters'].name;
+        return (field['component-parameters'] && field['component-parameters'].label) ||
+            (field['component-parameters'].InputLabelProps && field['component-parameters'].InputLabelProps.label) ||
+            field['component-parameters'].name;
     }
 
     const setFieldLabel = (newField: any, label: string) => {
@@ -38,7 +38,7 @@ export const BaseFieldEditor = ({fieldName, children}) => {
     }
 
     const updateField = (fieldName: string, newField: any) => {
-        dispatch({type: 'ui-specification/fieldUpdated', payload: {fieldName, newField}})        
+        dispatch({ type: 'ui-specification/fieldUpdated', payload: { fieldName, newField } })
     }
 
     const cParams = field['component-parameters'];
@@ -46,79 +46,118 @@ export const BaseFieldEditor = ({fieldName, children}) => {
     const state = {
         label: getFieldLabel(),
         helperText: cParams.helperText || "",
-        required: cParams.required || false, 
+        required: cParams.required || false,
         annotation: field.meta.annotation || false,
-        uncertainty: field.meta.uncertainty.include || false
+        annotationLabel: field.meta.annotation_label || "",
+        uncertainty: field.meta.uncertainty.include || false,
+        uncertaintyLabel: field.meta.uncertainty.label || ""
     };
 
-    const updateFieldFromState = (newState) => {
+    // gets rid of the type error in updateFieldFromState func
+    type newState = {
+        label: string,
+        helperText: string,
+        required: boolean,
+        annotation: boolean,
+        annotationLabel: string,
+        uncertainty: boolean,
+        uncertaintyLabel: string
+    }
+
+    const updateFieldFromState = (newState: newState) => {
         const newField = JSON.parse(JSON.stringify(field)); // deep copy
         setFieldLabel(newField, newState.label);
         newField['component-parameters'].helperText = newState.helperText;
         newField['component-parameters'].required = newState.required;
         newField.meta.annotation = newState.annotation;
-        newField.meta.uncertainty = {include: newState.uncertainty, label: "uncertainty"};
+        newField.meta.annotation_label = newState.annotationLabel;
+        newField.meta.uncertainty.include = newState.uncertainty;
+        newField.meta.uncertainty.label = newState.uncertaintyLabel;
         updateField(fieldName, newField);
     };
 
     const updateProperty = (prop: string, value: any) => {
-        const newState = {...state, [prop]: value};
+        const newState = { ...state, [prop]: value };
         updateFieldFromState(newState);
     };
 
-    return ( 
-            <Grid container spacing={2}>
-                <Grid item sm={6} xs={12}>
-                    <TextField 
-                        name="label" 
+    return (
+        <Grid container spacing={2}>
+            <Grid item sm={6} xs={12}>
+                <TextField
+                    name="label"
+                    variant="outlined"
+                    label="Label"
+                    value={state.label}
+                    onChange={(e) => updateProperty('label', e.target.value)}
+                    helperText="Enter a label for the field."
+                />
+            </Grid>
+
+            <Grid item sm={6} xs={12}>
+                <TextField
+                    name="helperText"
+                    variant="outlined"
+                    label="Helper Text"
+                    fullWidth
+                    multiline={true}
+                    rows={4}
+                    value={state.helperText}
+                    helperText="Help text shown along with the field (like this text)."
+                    onChange={(e) => updateProperty('helperText', e.target.value)}
+                />
+            </Grid>
+
+            {children}
+
+            <Grid item sm={6}>
+                <FormControlLabel required
+                    control={<Checkbox
+                        checked={state.required}
+                        onChange={(e) => updateProperty('required', e.target.checked)}
+                    />} label="Required" />
+            </Grid>
+
+            <Grid item sm={6} container direction="column">
+                <FormControlLabel required
+                    control={<Checkbox
+                        checked={state.annotation}
+                        onChange={(e) => updateProperty('annotation', e.target.checked)}
+                    />} label="Enable Annotation" />
+
+                {state.annotation &&
+                    <TextField
+                        name="label"
                         variant="outlined"
                         label="Label"
-                        value={state.label} 
-                        onChange={(e) => updateProperty('label', e.target.value)} 
-                        helperText="Enter a label for the field"
+                        value={state.annotationLabel}
+                        onChange={(e) => updateProperty('annotationLabel', e.target.value)}
+                        helperText="Enter a label."
+                        sx={{ mt: 1.5 }}
                     />
-                </Grid>
+                }
+            </Grid>
 
-                <Grid item sm={6} xs={12}>
-                    <TextField 
-                        name="helperText" 
+            <Grid item sm={6} container direction="column">
+                <FormControlLabel required
+                    control={<Checkbox
+                        checked={state.uncertainty}
+                        onChange={(e) => updateProperty('uncertainty', e.target.checked)}
+                    />} label="Enable Uncertainty" />
+
+                {state.uncertainty &&
+                    <TextField
+                        name="label"
                         variant="outlined"
-                        label="Helper Text"
-                        fullWidth
-                        multiline={true}
-                        rows={4}
-                        value={state.helperText} 
-                        helperText="Help text shown along with the field (like this text)."
-                        onChange={(e) => updateProperty('helperText', e.target.value)} 
+                        label="Label"
+                        value={state.uncertaintyLabel}
+                        onChange={(e) => updateProperty('uncertaintyLabel', e.target.value)}
+                        helperText="Enter a label."
+                        sx={{ mt: 1.5 }}
                     />
-                </Grid>
-
-                {children}
-
-                <Grid item sm={6}>
-                    <FormControlLabel required 
-                                control={<Checkbox 
-                                checked={state.required}
-                                onChange={(e) => updateProperty('required', e.target.checked)} 
-                              />} label="Required" />
-                </Grid>
-
-                <Grid item sm={6}>
-                    <FormControlLabel required 
-                    control={<Checkbox 
-                                checked={state.annotation}
-                                onChange={(e) => updateProperty('annotation', e.target.checked)} 
-                              />} label="Enable Annotation" />
-                </Grid>
-
-                <Grid item sm={6}>
-                    <FormControlLabel required 
-                    control={<Checkbox 
-                                checked={state.uncertainty}
-                                onChange={(e) => updateProperty('uncertainty', e.target.checked)} 
-                              />} label="Enable Uncertainty" />
-                </Grid>
-            </Grid> 
+                }
+            </Grid>
+        </Grid>
     )
 
 };
