10fb94ffdd9d44486426d35adb74e5a7eedde4a6
Paola <paolareategui@hotmail.com>
2024-03-28 21:02:13 +1100

    Add persistent boolean to ComponentParameters

--- src/state/initial.ts
+++ src/state/initial.ts
@@ -14,120 +14,124 @@
 
 import { ConditionType } from "../components/condition";
 
-
 export type NotebookMetadata = PropertyMap;
 
 export type PropertyMap = {
-    [key: string]: unknown 
+  [key: string]: unknown;
 };
 
 export type ComponentParameters = {
-    fullWidth?: boolean,
-    name?: string,
-    id?: string,
-    helperText?: string,
-    variant?: string,
-    label?: string,
-    multiline?: boolean,
-    multiple?: boolean,
-    SelectProps?: unknown,
-    ElementProps?: { 
-        options?: {
-            value: string, 
-            label: string, 
-            RadioProps?: unknown,
-        }[],
-        optiontree?: unknown,
-    },
-    InputLabelProps?: {label: string},
-    InputProps?: {rows?: number, type?: string},
-    FormLabelProps?: {children?: string},
-    FormHelperTextProps?: {children?: string},
-    FormControlLabelProps?: {label: string},
-    initialValue?: unknown,
-    related_type?: string,
-    relation_type?: string,
-    related_type_label?: string,
-    relation_linked_vocabPair?: [string, string][],
-    required?: boolean,
-    template?: string,
-    num_digits?: number,
-    form_id?: string,
-    is_auto_pick?: boolean,
-    zoom?: number,
-    featureType?: string,
-    variant_style?: string,
-    html_tag?: string,
-    content?: string,
-    hrid?: boolean,
-    select?: boolean,
-    geoTiff?: string,
-    type?: string,
-    valuetype?: string,
+  fullWidth?: boolean;
+  name?: string;
+  id?: string;
+  helperText?: string;
+  variant?: string;
+  label?: string;
+  multiline?: boolean;
+  multiple?: boolean;
+  SelectProps?: unknown;
+  ElementProps?: {
+    options?: {
+      value: string;
+      label: string;
+      RadioProps?: unknown;
+    }[];
+    optiontree?: unknown;
+  };
+  InputLabelProps?: { label: string };
+  InputProps?: { rows?: number; type?: string };
+  FormLabelProps?: { children?: string };
+  FormHelperTextProps?: { children?: string };
+  FormControlLabelProps?: { label: string };
+  initialValue?: unknown;
+  related_type?: string;
+  relation_type?: string;
+  related_type_label?: string;
+  relation_linked_vocabPair?: [string, string][];
+  required?: boolean;
+  persistent?: boolean;
+  template?: string | false;
+  num_digits?: number;
+  form_id?: string;
+  is_auto_pick?: boolean;
+  zoom?: number;
+  featureType?: string;
+  variant_style?: string;
+  html_tag?: string;
+  content?: string;
+  hrid?: boolean;
+  select?: boolean;
+  geoTiff?: string;
+  type?: string;
+  valuetype?: string;
 };
 
-export type ValidationSchemaElement =  (string | number)[];
+export type ValidationSchemaElement = (string | number)[];
 
 export type FieldType = {
-    "component-namespace": string,
-    "component-name": string,
-    "type-returned": string,
-    "component-parameters": ComponentParameters,
-    "validationSchema"?: ValidationSchemaElement[],
-    "initialValue"?: unknown,
-    "access"?: string[],
-    "condition"?: ConditionType | null,
-    "meta"?: {
-        "annotation_label": string,
-        "annotation": boolean,
-        "uncertainty": {
-            "include" : boolean,
-            "label": string,
-        }
-    }
-}; 
+  "component-namespace": string;
+  "component-name": string;
+  "type-returned": string;
+  "component-parameters": ComponentParameters;
+  validationSchema?: ValidationSchemaElement[];
+  initialValue?: unknown;
+  access?: string[];
+  condition?: ConditionType | null;
+  meta?: {
+    annotation_label: string;
+    annotation: boolean;
+    uncertainty: {
+      include: boolean;
+      label: string;
+    };
+  };
+};
 
 export type NotebookUISpec = {
-    fields: {[key: string]: FieldType},
-    fviews: {[key: string]: {
-        "fields": string[],
-        "uidesign"?: string,
-        "label": string,
-        "condition"?: ConditionType,
-    }},
-    viewsets: {[key: string]: {
-        "views": string[],
-        "label": string
-    }},
-    visible_types: string[],
-}
+  fields: { [key: string]: FieldType };
+  fviews: {
+    [key: string]: {
+      fields: string[];
+      uidesign?: string;
+      label: string;
+      condition?: ConditionType;
+    };
+  };
+  viewsets: {
+    [key: string]: {
+      views: string[];
+      label: string;
+    };
+  };
+  visible_types: string[];
+};
 
 export type Notebook = {
-    metadata: NotebookMetadata,
-    "ui-specification": NotebookUISpec
-}
+  metadata: NotebookMetadata;
+  "ui-specification": NotebookUISpec;
+};
 
 // an empty notebook
 export const initialState: Notebook = {
-    "metadata": {
-        "notebook_version": "1.0",
-        "schema_version": "1.0",
-        "name": "",
-        "accesses": ["admin", "moderator", "team"],
-        "filenames": [],
-        "ispublic": false,
-        "isrequest": false,
-        "lead_institution": "",
-        "showQRCodeButton": false,
-        "pre_description": "",
-        "project_lead": "",
-        "project_status": "New",
-        "sections": {}
-    },
-    "ui-specification": {
-        "fields":{},
-        "fviews": {},
-        "viewsets": {},
-        "visible_types": []
-    }
-}
+  metadata: {
+    notebook_version: "1.0",
+    schema_version: "1.0",
+    name: "",
+    accesses: ["admin", "moderator", "team"],
+    filenames: [],
+    ispublic: false,
+    isrequest: false,
+    lead_institution: "",
+    showQRCodeButton: false,
+    pre_description: "",
+    project_lead: "",
+    project_status: "New",
+    sections: {},
+  },
+  "ui-specification": {
+    fields: {},
+    fviews: {},
+    viewsets: {},
+    visible_types: [],
+  },
+};
